5bc84153a3a560b4c7201456afa4efceab9cc21f
殷小杰 <yin7968@Yins-MacBook-Pro.local>
2021-05-17 20:22:09 +0800

    update networks.py

--- src/networks.py
+++ src/networks.py
@@ -103,9 +103,11 @@ def save_best_networks(networks, result_dir):
 
 def get_optimizers(networks, lr=.0001, beta1=.5, beta2=.999, weight_decay=.0, finetune=False, **options):
     optimizers = {}
-    if finetune:
-        lr /= 10
-        print("Fine-tuning mode activated, dropping learning rate to {}".format(lr))
+    # if finetune:
+    #     lr /= 10
+    #     print("Fine-tuning mode activated, dropping learning rate to {}".format(lr))
+    lr /= 10
+    print("Fine-tuning mode activated, dropping learning rate to {}".format(lr))
     for name in networks:
         net = networks[name]
         optimizers[name] = optim.Adam(net.parameters(), lr=lr, betas=(beta1, beta2), weight_decay=weight_decay)
